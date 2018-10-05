8d406801afa644fab8bc3cedf72d248d0680f553
Rohit Kumar <rohit9075@gmail.com>
2018-10-05 11:19:53 +0530

    project Gradle update

--- app/src/main/java/com/rohit/fragmentstability/MainActivity.java
+++ app/src/main/java/com/rohit/fragmentstability/MainActivity.java
@@ -3,15 +3,7 @@ package com.rohit.fragmentstability;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-/**
- *
- * Author : Sriyank Siddhartha
- *
- * 		Module 9: Providing stability to Fragment on Screen Rotation
- *
- * 		"AFTER" Demo Project
- *
- * */
+
 public class MainActivity extends AppCompatActivity {
 
 	private static final String TAG = MainActivity.class.getSimpleName();
